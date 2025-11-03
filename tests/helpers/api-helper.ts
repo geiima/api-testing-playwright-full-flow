79909c509ee4f7c96c5f3bdf160dbdb1ffcea08f
Eimante.Bogdan <eimante.bogdan@gmail.com>
2025-11-03 08:48:41 +0200

    Added new functions (getTwoLastOrders, getAllOrders) and created new test cases for these functions

--- tests/helpers/api-helper.ts
+++ tests/helpers/api-helper.ts
@@ -2,7 +2,16 @@ import { APIRequestContext, expect } from '@playwright/test'
 import { StatusCodes } from 'http-status-codes'
 import { LoginDto } from '../dto/login-dto'
 import { OrderDto } from '../dto/order-dto'
+import { StatusDto } from '../dto/status-dto'
 
+type Order = {
+  "status": StatusDto,
+  "courierId": number,
+  "customerName": string,
+  "customerPhone": string,
+  "comment": string,
+  "id": number
+}
 const serviceURL = 'https://backend.tallinn-learning.ee/'
 const loginPath = 'login/student'
 const orderPath = 'orders'
@@ -77,4 +86,51 @@ export async function getDeletedOrder(
   expect(response.status()).toBe(StatusCodes.OK)
   const data = await response.text()
   expect(data).toBe('')
+}
+
+export async function getTwoLastOrders(
+  request: APIRequestContext,
+  jwt: string,
+): Promise<OrderDto[]> {
+  const response = await request.get(`${serviceURL}${orderPath}`, {
+    headers: {
+      Authorization: `Bearer ${jwt}`,
+    },
+  })
+  expect(response.status()).toBe(StatusCodes.OK)
+  const data: Order[] = await response.json()
+
+  const lastIndex = data.length - 1
+  const secondToLastIndex = data.length - 2
+
+  const item1 = data[secondToLastIndex]
+  const item2 = data[lastIndex]
+
+  const order1 = new OrderDto(
+    item1.status,
+    item1.courierId,
+    item1.customerName,
+    item1.customerPhone,
+    item1.comment,
+    item1.id,
+  )
+  const order2 = new OrderDto(
+    item2.status,
+    item2.courierId,
+    item2.customerName,
+    item2.customerPhone,
+    item2.comment,
+    item2.id,
+  )
+  return [order1, order2]
+}
+
+export async function getAllOrders(request: APIRequestContext, jwt: string): Promise<OrderDto[]> {
+  const response = await request.get(`${serviceURL}${orderPath}`, {
+    headers: {
+      Authorization: `Bearer ${jwt}`,
+    },
+  })
+  expect(response.status()).toBe(StatusCodes.OK)
+  return await response.json()
 }
--- tests/specs/api/auth-order-with-helper.spec.ts
+++ tests/specs/api/auth-order-with-helper.spec.ts
@@ -5,9 +5,13 @@ import {
   getOrderById,
   deleteOrder,
   getDeletedOrder,
+  getAllOrders,
+  getTwoLastOrders,
 } from '../../helpers/api-helper'
 import { StatusDto } from '../../dto/status-dto'
 import { OrderDto } from '../../dto/order-dto'
+
+test.describe.configure({ mode: 'serial' })
 
 // defined on test file level for all
 let jwt: string
@@ -31,7 +35,37 @@ test('create order and find order by id', async ({ request }) => {
 
 test('create order and delete order by id and get deleted order', async ({ request }) => {
   const orderId = await createOrder(request, jwt)
-  console.log(orderId)
+  //console.log(orderId)
   await deleteOrder(request, jwt, orderId)
   await getDeletedOrder(request, jwt, orderId)
+})
+
+test('Create two orders and get two last orders from all the list', async ({ request }) => {
+  const orderIdFirst = await createOrder(request, jwt)
+  const orderIdSecond = await createOrder(request, jwt)
+  const allOrders: OrderDto[] = await getTwoLastOrders(request, jwt)
+  expect(allOrders.length).toBe(2)
+
+  const receivedIds = [allOrders[0].id, allOrders[1].id]
+  expect(receivedIds).toContain(orderIdFirst)
+  expect(receivedIds).toContain(orderIdSecond)
+
+  expect.soft(allOrders[0].status).toBe(StatusDto.OPEN)
+  expect.soft(allOrders[1].status).toBe(StatusDto.OPEN)
+
+  //console.log(allOrders[0], allOrders[1])
+})
+
+test('Student creates order and verifies it in all orders list - the last one', async ({
+  request,
+}) => {
+  const orderId = await createOrder(request, jwt)
+  const retrieveOrder = await getOrderById(request, jwt, orderId)
+  expect(retrieveOrder.id).toBe(orderId)
+  expect(retrieveOrder.status).toBe(StatusDto.OPEN)
+
+  const allOrders = await getAllOrders(request, jwt)
+
+  const lastOrderIndex = allOrders.length - 1
+  expect(allOrders[lastOrderIndex].id).toBe(orderId)
 })
